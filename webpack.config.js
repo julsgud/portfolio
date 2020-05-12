fe5b20d737da8139f38fc20da9a26511670d3444
Julio Gudino <julsgud@gmail.com>
2020-05-12 18:09:11 -0500

    Change jsx

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,6 @@ const CopyWebpackPluginConfig = new CopyWebpackPlugin([
 ])
 
 module.exports = {
-  entry: "./src/index.js",
-  devServer: {
-    contentBase: "dist",
-  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
@@ -25,7 +21,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
